2893783366b78274fc3bcc9b16b3f007b087adce
Harding Liu <zongmingxliu@gmail.com>
2018-11-17 23:55:59 -0400

    Fix formatting

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 all: huftree
 
 huftree: huftree.o codetree.o
-	g++ -Wall -g -o huftree huftree.o codetree.o
+	g++ -std=c++11 -Wall -g -o huftree huftree.o codetree.o
 
 codetree.o: codetree.cc codetree.h
-	g++ -Wall -o codetree.o -c -g codetree.cc
+	g++ -std=c++11 -Wall -o codetree.o -c -g codetree.cc
 
 huftree.o: huftree.cc codetree.h
-	g++ -Wall -o huftree.o -c -g huftree.cc
+	g++ -std=c++11 -Wall -o huftree.o -c -g huftree.cc
 
 clean:
 	rm *.o
--- huftree.cc
+++ huftree.cc
@@ -5,30 +5,30 @@
 // Print the Huffman code tree and list of codes for a given text file.
 
 void usage() {
-    std::cerr << "Usage: huftree text.txt" << std::endl;
-    throw std::exception();
+  std::cerr << "Usage: huftree text.txt" << std::endl;
+  throw std::exception();
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        usage();
+  if (argc != 2) {
+    usage();
+  }
+
+  int freq[256] = {0}; // array initialized to 0
+
+  std::ifstream fin(argv[1]);
+  if (fin.is_open()) {
+    char ch;
+    while (fin >> std::noskipws >> ch) { // don't skip whitespace
+      freq[(int)ch]++;
     }
 
-    int freq[256] = {0}; // array initialized to 0
+    fin.close();
+  }
 
-    std::ifstream fin(argv[1]);
-    if (fin.is_open()) {
-        char ch;
-        while (fin >> std::noskipws >> ch) { // don't skip whitespace
-            freq[(int)ch]++;
-        }
+  CodeTree ct = CodeTree(freq);
 
-        fin.close();
-    }
-
-    CodeTree ct = CodeTree(freq);
-
-    ct.printTree();
-    ct.printCode();
-    return 0;
+  ct.printTree();
+  ct.printCode();
+  return 0;
 }
